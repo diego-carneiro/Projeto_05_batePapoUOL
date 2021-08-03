4612c3c033063881959355c1324b626d90def6df
diego-carneio <diegosantos.cs@gmail.com>
2021-08-03 15:46:13 -0300

    Início do Layout

---
+++ uolscript.js
@@ -0,0 +1,14 @@
+// let digitarMsg = `<div class="textBox">
+// <input type="text" name="msgtext" id="msgtext">
+// </div>`
+
+
+// function iniciarTexto(){
+//     let swap = document.querySelector('#escrevaAqui').innerHTML = digitarMsg;
+//     swap.classList.add("displayNone");
+// }
+// function restaurarCaixa(){
+//     let swap = document.querySelector('#escrevaAqui').innerHTML = digitarMsg;
+//     swap.classList.remove("displayNone");
+// }
+
